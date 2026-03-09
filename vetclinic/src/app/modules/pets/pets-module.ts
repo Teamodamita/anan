337fdf7c512295627108d680e92034252e3d9a5b
Teamodamita <eduardoabai2504@gmail.com>
2026-03-09 05:17:53 +0000

    models

---
+++ vetclinic/src/app/modules/pets/pets-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PetsRoutingModule } from './pets-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PetsRoutingModule],
+})
+export class PetsModule {}
