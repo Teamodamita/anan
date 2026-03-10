b76d930f98991b4c65490311ed5f0e6893943ca6
Teamodamita <eduardoabai2504@gmail.com>
2026-03-10 05:52:33 +0000

    rutas

--- vetclinic/src/app/app.routes.ts
+++ vetclinic/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES)
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.routes').then((m) => m.AUTH_ROUTES)
+  },
+  {
+    path: 'pets',
+    loadChildren: () =>
+      import('./modules/pets/pets.routes').then((m) => m.PETS_ROUTES)
+  },
+  {
+    path: 'appointments',
+    loadChildren: () =>
+      import('./modules/appointments/appointments.routes').then((m) => m.APPOINTMENTS_ROUTES)
+  },
+  {
+    path: 'history',
+    loadChildren: () =>
+      import('./modules/history/history.routes').then((m) => m.HISTORY_ROUTES)
+  },
+  { path: '**', redirectTo: '' }
+];
